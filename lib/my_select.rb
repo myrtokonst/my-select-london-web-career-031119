fd786defc046e2ff1bc42059533445a9eb50d445
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-05 21:23:43 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  selected=[]
  i=0
 while i<collection.length 
- if yield collection[i]== true 
+ if yield collection[i]
    selected.push(yield collection[i])
 end
 i=i+1 
