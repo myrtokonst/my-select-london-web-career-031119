a1ebbfdef5c81129c6ec97a1976ffbdb0790b1a1
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-05 21:22:29 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- # your code here!
+ selected=[]
+ i=0
+while i<collection.length 
+ if yield collection[i]
+   selected.push(yield collection[i])
+end
+end
+selected
 end
